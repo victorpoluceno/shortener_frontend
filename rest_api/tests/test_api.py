2d75d0027bfd13f752065db9a574e590afd1d519
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2011-07-30 00:41:02 -0300

    Test UrlThrottle class

--- rest_api/tests/test_api.py
+++ rest_api/tests/test_api.py
@@ -6,35 +6,13 @@ from django.test.client import Client
 
 from django.contrib.auth.models import User
 
+from rest_api.api import UrlThrottle
 
-class SmokeTest(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.base_url = '/api/v1/url/'
 
-        self.user = User.objects.create_user('tests', \
-                email='victorpoluceno@gmail.com')
-        self.user.is_superuser = True
-        self.user.is_staff = True
-        self.user.save()
+class ApiTest(TestCase):
+    def test_url_throttle(self):
+        from django.conf import settings
+        settings.SHOULD_BE_THROTTLE = False
 
-    def test_url_short_api(self):
-        auth_chunk = urllib.urlencode([('username', self.user.username), 
-                ('api_key', self.user.api_key.key)])
-
-        url = ''.join([self.base_url, '?', auth_chunk])
-        response = self.client.get(url, content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-
-        data = {'long_url': 'http://xxx.com'}
-        response = self.client.post(url, json.dumps(data), 'application/json')
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response['Location'], "http://testserver/api/v1/url/1/")
-
-        url = ''.join([self.base_url, '1/?', auth_chunk])
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        data = json.loads(response.content)
-
-        self.assertNotEqual(data['key'], '')
-        self.assertNotEqual(data['key'], None)
+        url_throttle = UrlThrottle()
+        self.assertEqual(url_throttle.should_be_throttled('x'), False)
